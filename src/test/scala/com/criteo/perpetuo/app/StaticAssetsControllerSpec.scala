43e31c3a80a3c12f6ea76c04b778a07193c41317
Hugues Lerebours <h.lerebours@criteo.com>
2016-12-15 12:34:13 +0100

    Run the tests
    They were not run: move them where they should be and fix them to work
    (add necessary imports and dependencies)
    
    Change-Id: I0f647265ff49ce52462d796acd8cb4c4ea0c3aef

--- src/test/scala/com/criteo/perpetuo/app/StaticAssetsControllerSpec.scala
+++ src/test/scala/com/criteo/perpetuo/app/StaticAssetsControllerSpec.scala
@@ -5,7 +5,10 @@ import com.twitter.finagle.http.{Request, Response}
 import com.twitter.finatra.http.HttpServer
 import com.twitter.finatra.http.filters.{CommonFilters, LoggingMDCFilter, TraceIdMDCFilter}
 import com.twitter.finatra.http.routing.HttpRouter
+import com.twitter.finatra.http.test.EmbeddedHttpServer
+import com.twitter.inject.server.FeatureTest
 import com.typesafe.config.{Config, ConfigFactory}
+
 
 /**
   * An integration test for [[StaticAssetsController]].
