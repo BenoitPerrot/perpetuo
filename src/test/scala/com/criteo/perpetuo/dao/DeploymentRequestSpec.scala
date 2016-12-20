0d19a78842a7b207cb6738ced77590d188cccbe7
Hugues Lerebours <h.lerebours@criteo.com>
2016-12-20 11:27:47 +0100

    Test DeploymenRequest binding
    Change-Id: I47dad31d7f77b19616fa7a72a41d00f3a769605d

---
+++ src/test/scala/com/criteo/perpetuo/dao/DeploymentRequestSpec.scala
@@ -0,0 +1,52 @@
+package com.criteo.perpetuo.dao
+
+import java.sql.Timestamp
+
+import com.typesafe.config.{Config, ConfigFactory}
+import org.junit.runner.RunWith
+import org.scalatest.FunSuite
+import org.scalatest.concurrent._
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.time.{Millis, Seconds, Span}
+import slick.driver.JdbcDriver
+
+import scala.concurrent.Await
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration._
+
+
+@RunWith(classOf[JUnitRunner])
+class DeploymentRequestSpec extends FunSuite with ScalaFutures
+  with DeploymentRequestBinder
+  with ProfileProvider {
+
+  implicit val defaultPatience = PatienceConfig(timeout = Span(2, Seconds), interval = Span(100, Millis))
+
+  val config: Config = ConfigFactory.load()
+  val dbModule = new TestingDbContextModule(config.getConfig("db").getConfig("embedded"))
+
+  val profile: JdbcDriver = dbModule.driver
+
+  import profile.api._
+
+  val db: profile.backend.DatabaseDef = Database.forDataSource(dbModule.dataSourceProvider)
+
+  val schemaCreation = DBIO.seq(
+    deploymentRequestQuery.schema.create
+  )
+  Await.result(db.run(schemaCreation), 2.second)
+
+  test("Deployment requests can be inserted and retrieved") {
+    val request = DeploymentRequest(None, "perpetuo-app", "v42", "*", "No fear", "c.norris", new Timestamp(123456789))
+
+    Await.result(for {
+      id <- insert(db, request)
+      requests <- db.run(deploymentRequestQuery.result)
+      lookup <- findDeploymentRequestById(db, id)
+    } yield {
+      assert(requests.nonEmpty)
+      assert(lookup.isDefined)
+      assert(lookup.get == request.copy(id = Some(id)))
+    }, 2.seconds)
+  }
+}
