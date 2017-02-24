55c9c2d588278c04345fba632fee66674f87ffa9
Hugues Lerebours <h.lerebours@criteo.com>
2017-02-24 12:08:23 +0100

    [itest framework] it can use already running Perpetuo
    very handy:
    you can run Perpetuo in your IDE,
    run the itests in your console,
    watch the server logs in your IDE
    
    Change-Id: I3e70b315e0c6a236509c118914e614974702d01b

--- src/test/integration-test-suite.sh
+++ src/test/integration-test-suite.sh
@@ -4,13 +4,6 @@ set -eu
 
 cd $(dirname $0)/../..
 
-mvn clean package -DpackageForDeploy -DskipTests
-perpetuo_jar=$(ls -t target/perpetuo-app-*-uber.jar | head -1)
-echo Using ${perpetuo_jar}
-trap_stack="" # used by `start_temporarily`
-start_temporarily "Perpetuo" "Startup complete, server ready" java -Dtokens.rundeck=token -jar ${perpetuo_jar}
-echo
-
 function api_query() {
     echo "==> /api/$@" 1>&2
     echo $(curl -sfS -H 'Content-Type: application/json' "http://localhost:8989/api/$@")
@@ -21,8 +14,22 @@ function expects() {
     grep -Ex "$1" <<< "${line}" || { echo "! Expected $1"; echo ". Got ${line}"; return 1; }
 }
 
-# setup
-api_query products -d '{"name": "itest-project"}'
+
+### setup
+if ! curl -fs localhost:8989 -o /dev/null
+then
+    mvn clean package -DpackageForDeploy -DskipTests
+    perpetuo_jar=$(ls -t target/perpetuo-app-*-uber.jar | head -1)
+    echo Using ${perpetuo_jar}
+    trap_stack="" # used by `start_temporarily`
+    start_temporarily "Perpetuo" "Startup complete, server ready" java -Dtokens.rundeck=token -jar ${perpetuo_jar}
+    echo
+
+    api_query products -d '{"name": "itest-project"}'
+else
+    echo "Perpetuo seems to run already. Using it."
+    api_query products -d '{"name": "itest-project"}' 2> /dev/null || true
+fi
 
 
 ### scenarios
