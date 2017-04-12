f85f2a9185f4f28143c55d347cb0bce8bc9f69f5
Hugues Lerebours <h.lerebours@criteo.com>
2017-04-12 17:17:37 +0200

    Provide logging facility in hooks
    Change-Id: I9635bc43556f03fdab9f928866fe8137e0775323

--- src/main/scala/com/criteo/perpetuo/config/Hooks.scala
+++ src/main/scala/com/criteo/perpetuo/config/Hooks.scala
@@ -1,5 +1,7 @@
 package com.criteo.perpetuo.config
 
+import java.util.logging.Logger
+
 import com.criteo.perpetuo.model.DeploymentRequest
 
 
@@ -10,4 +12,27 @@ class Hooks {
   def onDeploymentRequestCreated(deploymentRequest: DeploymentRequest): Unit = {}
 
   def onDeploymentRequestStarted(deploymentRequest: DeploymentRequest, immediately: Boolean): Unit = {}
+
+
+  /** This logger is the one to use in the Groovy hook classes */
+  protected val logger: Logger = Logger.getLogger("hooks")
+
+
+  /**
+    * Helpers to catch all errors in a hook and only log them.
+    *
+    * @param operation the piece of code to be fail-safely executed
+    */
+  def quietlyLogErrors[T](hookName: String, operation: Runnable): Unit = {
+    try {
+      operation.run()
+    } catch {
+      case e: Throwable =>
+        val prefix = if (hookName.isEmpty) "" else s"[$hookName] "
+        logger.severe(s"$prefix${e.getMessage}\n${e.getStackTrace.mkString("\n")}")
+    }
+  }
+
+  // we define two methods instead of one having an optional parameter `hookName`, for an easier use in Groovy scripts.
+  def quietlyLogErrors(operation: Runnable): Unit = quietlyLogErrors("", operation)
 }
