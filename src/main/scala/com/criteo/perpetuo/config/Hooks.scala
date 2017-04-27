9909b5153b6d36deecd5b865540371f45c7931d5
Hugues Lerebours <h.lerebours@criteo.com>
2017-04-27 15:24:22 +0200

    [refacto] base plugin trait and class, honoring default implementation
    Change-Id: I75b837ca4dbd88a9829e0312cf2cf86e66b5f28e

--- src/main/scala/com/criteo/perpetuo/config/Hooks.scala
+++ src/main/scala/com/criteo/perpetuo/config/Hooks.scala
@@ -1,79 +1,40 @@
 package com.criteo.perpetuo.config
 
-import java.lang.reflect.InvocationTargetException
-import java.util.logging.Logger
-
 import com.criteo.perpetuo.model.DeploymentRequest
 
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.duration._
-import scala.concurrent.{Await, Future}
+import scala.concurrent.Future
 
 
 // fixme: temporarily parametrized
-trait ImplementableHooks[T] {
+trait BaseHooks[T] {
   def onDeploymentRequestCreated(deploymentRequest: DeploymentRequest, immediateStart: Boolean): T
 
-  def onDeploymentRequestStarted(deploymentRequest: DeploymentRequest, startedExecutions: Int, failedToStart: Int, immediately: Boolean)
+  def onDeploymentRequestStarted(deploymentRequest: DeploymentRequest, startedExecutions: Int, failedToStart: Int, immediately: Boolean): Unit
 }
 
 
-class Hooks extends ImplementableHooks[String] {
+class Hooks extends BaseHooks[String] with Plugin {
   /**
     * Methods that can be overridden as hooks.
     */
   def onDeploymentRequestCreated(deploymentRequest: DeploymentRequest, immediateStart: Boolean): String = null
 
   def onDeploymentRequestStarted(deploymentRequest: DeploymentRequest, startedExecutions: Int, failedToStart: Int, immediately: Boolean): Unit = {}
-
-
-  /**
-    * This logger is the one to use in the Groovy hook classes, and it should not be overridden
-    */
-  val logger: Logger = Logger.getLogger("hooks")
-
-  /**
-    * Timeout applicable on each hook, in seconds; can be overridden in Groovy hook classes
-    */
-  val timeout_s: Int = 30
 }
 
 
-private[config] class HooksTrigger(implementation: Option[Hooks]) extends ImplementableHooks[Future[String]] {
-  private val hooks: Hooks = implementation.getOrElse(new Hooks)
-
-  def onDeploymentRequestCreated(deploymentRequest: DeploymentRequest, immediateStart: Boolean): Future[String] =
-    wrap("onDeploymentRequestCreated", deploymentRequest, immediateStart)
+private[config] class HooksTrigger(implementation: Option[Hooks]) extends PluginRunner(implementation, new Hooks) with BaseHooks[Future[String]] {
+  override def onDeploymentRequestCreated(deploymentRequest: DeploymentRequest, immediateStart: Boolean): Future[String] =
+    inFuture("onDeploymentRequestCreated", deploymentRequest, immediateStart)
 
   def onDeploymentRequestStarted(deploymentRequest: DeploymentRequest, startedExecutions: Int, failedToStart: Int, immediately: Boolean): Unit =
-    wrap("onDeploymentRequestStarted", deploymentRequest, startedExecutions, failedToStart, immediately)
+    inFuture("onDeploymentRequestStarted", deploymentRequest, startedExecutions, failedToStart, immediately)
 
-  private def wrap(methodName: String, args: Any*): Future[String] = {
-    val method = hooks.getClass.getMethods.filter(_.getName == methodName).head
-    if (method.getDeclaringClass != classOf[Hooks]) {
-      // there is a specific implementation for this hook, let's start it in a background thread after logging its name
-      hooks.logger.info(s"$methodName")
-      Future {
-        var ret: String = null
-        try {
-          ret = Await.result(
-            Future {
-              method.invoke(hooks, args.map(_.asInstanceOf[AnyRef]): _*)
-            },
-            hooks.timeout_s.seconds
-          ).asInstanceOf[String]
-        }
-        catch {
-          case e: InvocationTargetException => logException(e.getCause, methodName)
-          case e: Throwable => logException(e, methodName)
-        }
-        ret
-      }
+  protected def inFuture[T](methodName: String, args: Any*): Future[T] = {
+    // start every hook method in a background thread
+    Future {
+      wrap(methodName, args: _*)
     }
-    else
-      Future.successful(null)
   }
-
-  private def logException(exc: Throwable, methodName: String) =
-    hooks.logger.severe(s"$methodName - ${exc.getMessage}\n${exc.getStackTrace.mkString("\n")}")
 }
--- src/main/scala/com/criteo/perpetuo/config/Plugins.scala
+++ src/main/scala/com/criteo/perpetuo/config/Plugins.scala
@@ -1,15 +1,21 @@
 package com.criteo.perpetuo.config
 
 import java.io.InputStreamReader
+import java.lang.reflect.{InvocationTargetException, Method}
+import java.util.logging.Logger
 import javax.script.{ScriptEngine, ScriptEngineManager}
 
 import com.criteo.perpetuo.dao.DbBinding
 import com.criteo.perpetuo.dispatchers.TargetDispatcher
 
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration._
+import scala.concurrent.{Await, Future}
+
 
 class Plugins(dbBinding: DbBinding, appConfig: BaseAppConfig = AppConfig) {
   lazy val dispatcher: TargetDispatcher = instantiateFromGroovy(AppConfig.get("plugins.dispatcher"))
-  lazy val hooks: HooksTrigger = new HooksTrigger(AppConfig.tryGet("plugins.hooks").map(instantiateFromGroovy[Hooks](_: String)))
+  lazy val hooks: HooksTrigger = new HooksTrigger(AppConfig.tryGet("plugins.hooks").map(instantiateFromGroovy[Hooks]))
 
 
   private val factory = new ScriptEngineManager
@@ -22,4 +28,52 @@ class Plugins(dbBinding: DbBinding, appConfig: BaseAppConfig = AppConfig) {
     val ctor = cls.getConstructor(classOf[DbBinding], classOf[RootAppConfig])
     ctor.newInstance(dbBinding, appConfig)
   }
+}
+
+
+trait Plugin {
+  /**
+    * This logger is the one to use in the Groovy classes, and it should not be overridden
+    */
+  val logger: Logger = Logger.getLogger(getClass.getName)
+
+  /**
+    * Timeout applicable on each plugin method, in seconds; can be overridden in Groovy classes
+    */
+  val timeout_s: Int = 30
+}
+
+
+abstract class PluginRunner[P <: Plugin](implementation: Option[P], base: P) {
+  protected val plugin: P = implementation.getOrElse(base)
+
+  protected def wrap[T](methodName: String, args: Any*): T = {
+    val method = plugin.getClass.getMethods.filter(_.getName == methodName).head
+    if (method.getDeclaringClass != base.getClass) {
+      // there is a specific implementation for this plugin method, let's say it and start it, but time-boxed
+      plugin.logger.info(s"$methodName")
+      Await.result(Future {
+        invoke(method, args: _*)
+      }, plugin.timeout_s.seconds)
+    }
+    else
+      invoke(method, args: _*)
+  }
+
+  private def invoke[T](method: Method, args: Any*): T =
+    try {
+      val res = try {
+        method.invoke(plugin, args.map(_.asInstanceOf[AnyRef]): _*)
+      }
+      catch {
+        case e: InvocationTargetException => throw e.getCause // redirect to the real error
+      }
+      res.asInstanceOf[T]
+    }
+    catch {
+      case e: Throwable =>
+        // to know which hook is failing, we prefix the trace
+        plugin.logger.severe(s"${method.getName} - ${e.getMessage}\n${e.getStackTrace.mkString("\n")}")
+        throw e // fixme: this is only as long as we return something (it's very temporary)
+    }
 }
