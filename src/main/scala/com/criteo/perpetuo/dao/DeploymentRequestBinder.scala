18d9ad12e6cd0980b6266b05e93f144542f11936
Benoit Perrot <be.perrot@criteo.com>
2017-08-09 11:57:09 +0200

    DeploymentRequestBinder: Cosmetics
    Change-Id: Ic38913e9d354ebf600aabdc8d1f10f3149c2d4b7

--- src/main/scala/com/criteo/perpetuo/dao/DeploymentRequestBinder.scala
+++ src/main/scala/com/criteo/perpetuo/dao/DeploymentRequestBinder.scala
@@ -68,7 +68,7 @@ trait DeploymentRequestBinder extends TableBinder {
   def findDeploymentRequestByIdWithProduct(id: Long): Future[Option[DeploymentRequest]] = {
     dbContext.db.run((deploymentRequestQuery join productQuery on (_.productId === _.id) filter (_._1.id === id)).result)
       .map(_.headOption.map {
-        case (req, prod) => DeploymentRequest(req.id.get, prod.toProduct, req.version, req.target, req.comment, req.creator, req.creationDate)
+        case (req, prod) => req.toDeploymentRequest(prod.toProduct)
       })
   }
 
