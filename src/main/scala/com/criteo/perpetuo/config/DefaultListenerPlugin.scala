46fd2ecc635e316dc443b8f1ff6d5bcd78710606
Hugues Lerebours <h.lerebours@criteo.com>
2017-08-18 18:11:47 +0200

    Add retry and rollback callbacks in the listener
    JIRA: DREDD-473, DREDD-474
    Change-Id: Ia7768452decbf410bfa37efa358ae5aed0e23d04

--- src/main/scala/com/criteo/perpetuo/config/DefaultListenerPlugin.scala
+++ src/main/scala/com/criteo/perpetuo/config/DefaultListenerPlugin.scala
@@ -9,6 +9,10 @@ class DefaultListenerPlugin extends Listener with Plugin {
 
   def onDeploymentRequestStarted(deploymentRequest: DeploymentRequest, startedExecutions: Int, failedToStart: Int, atCreation: Boolean): Unit = {}
 
+  def onDeploymentRequestRetried(deploymentRequest: DeploymentRequest, startedExecutions: Int, failedToStart: Int): Unit = {}
+
+  def onDeploymentRequestRolledBack(deploymentRequest: DeploymentRequest, startedExecutions: Int, failedToStart: Int): Unit = {}
+
   def onOperationClosed(operationTrace: OperationTrace, deploymentRequest: DeploymentRequest, succeeded: Boolean): Unit = {}
 
   val timeout_s = 30
@@ -21,6 +25,12 @@ private[config] class ListenerPluginWrapper(implementation: Option[DefaultListen
 
   def onDeploymentRequestStarted(deploymentRequest: DeploymentRequest, startedExecutions: Int, failedToStart: Int, atCreation: Boolean): Unit =
     wrap(_.onDeploymentRequestStarted(deploymentRequest, startedExecutions, failedToStart, atCreation))
+
+  def onDeploymentRequestRetried(deploymentRequest: DeploymentRequest, startedExecutions: Int, failedToStart: Int): Unit =
+    wrap(_.onDeploymentRequestRetried(deploymentRequest, startedExecutions, failedToStart))
+
+  def onDeploymentRequestRolledBack(deploymentRequest: DeploymentRequest, startedExecutions: Int, failedToStart: Int): Unit =
+    wrap(_.onDeploymentRequestRolledBack(deploymentRequest, startedExecutions, failedToStart))
 
   def onOperationClosed(operationTrace: OperationTrace, deploymentRequest: DeploymentRequest, succeeded: Boolean): Unit =
     wrap(_.onOperationClosed(operationTrace, deploymentRequest, succeeded))
--- src/main/scala/com/criteo/perpetuo/engine/Listener.scala
+++ src/main/scala/com/criteo/perpetuo/engine/Listener.scala
@@ -7,5 +7,9 @@ trait Listener {
 
   def onDeploymentRequestStarted(deploymentRequest: DeploymentRequest, startedExecutions: Int, failedToStart: Int, atCreation: Boolean): Unit
 
+  def onDeploymentRequestRetried(deploymentRequest: DeploymentRequest, startedExecutions: Int, failedToStart: Int): Unit
+
+  def onDeploymentRequestRolledBack(deploymentRequest: DeploymentRequest, startedExecutions: Int, failedToStart: Int): Unit
+
   def onOperationClosed(operationTrace: OperationTrace, deploymentRequest: DeploymentRequest, succeeded: Boolean): Unit
 }
