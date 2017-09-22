be790b031fd73e5926633b40fc48cc28e8a10ed4
Hugues Lerebours <h.lerebours@criteo.com>
2017-09-22 14:10:50 +0200

    cosmetics: don't import the value type of an enumeration
    when it doesn't have an explicit name :)
    
    Change-Id: Ibfe85c9bc851c6998318f5bb1bd0ea835e679d80

--- src/main/scala/com/criteo/perpetuo/auth/Permissions.scala
+++ src/main/scala/com/criteo/perpetuo/auth/Permissions.scala
@@ -1,6 +1,6 @@
 package com.criteo.perpetuo.auth
 
-import com.criteo.perpetuo.model.Operation.Kind
+import com.criteo.perpetuo.model.Operation
 
 
 object GeneralAction extends Enumeration {
@@ -18,12 +18,12 @@ object DeploymentAction extends Enumeration {
 trait Permissions {
   def isAuthorized(username: String, action: GeneralAction.Value): Boolean
 
-  def isAuthorized(username: String, action: DeploymentAction.Value, operation: Kind, productName: String, target: Iterable[String]): Boolean
+  def isAuthorized(username: String, action: DeploymentAction.Value, operation: Operation.Kind, productName: String, target: Iterable[String]): Boolean
 }
 
 
 class Unrestricted extends Permissions {
   override def isAuthorized(username: String, action: GeneralAction.Value) = true
 
-  override def isAuthorized(username: String, action: DeploymentAction.Value, operation: Kind, productName: String, target: Iterable[String]) = true
+  override def isAuthorized(username: String, action: DeploymentAction.Value, operation: Operation.Kind, productName: String, target: Iterable[String]) = true
 }
--- src/main/scala/com/criteo/perpetuo/auth/PermissionsByOperationAndUsername.scala
+++ src/main/scala/com/criteo/perpetuo/auth/PermissionsByOperationAndUsername.scala
@@ -1,7 +1,7 @@
 package com.criteo.perpetuo.auth
 
 import com.criteo.perpetuo.config.AppConfig
-import com.criteo.perpetuo.model.Operation.Kind
+import com.criteo.perpetuo.model.Operation
 
 
 class PermissionsByOperationAndUsername(config: AppConfig) extends Permissions {
@@ -11,7 +11,7 @@ class PermissionsByOperationAndUsername(config: AppConfig) extends Permissions {
       .forall { allowed: Seq[String] => allowed.contains(username) }
   }
 
-  override def isAuthorized(username: String, action: DeploymentAction.Value, operation: Kind, productName: String, target: Iterable[String]): Boolean = {
+  override def isAuthorized(username: String, action: DeploymentAction.Value, operation: Operation.Kind, productName: String, target: Iterable[String]): Boolean = {
     val prefix = action match {
       case DeploymentAction.requestOperation => "request"
       case DeploymentAction.applyOperation => "apply"
