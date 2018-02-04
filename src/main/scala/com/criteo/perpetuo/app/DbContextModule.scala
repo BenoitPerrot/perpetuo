fc2c0d49b1d65edf198e7ea574290d57eba5167b
Benoit Perrot <benoit.noe.perrot@gmail.com>
2018-02-04 15:11:07 +0100

    app: (DbContextModule) Leverage Hikari
    Change-Id: Ida13b37544ca6844cfad49285658778a9e4e15cc
    JIRA: DREDD-657

--- src/main/scala/com/criteo/perpetuo/app/DbContextModule.scala
+++ src/main/scala/com/criteo/perpetuo/app/DbContextModule.scala
@@ -5,7 +5,8 @@ import com.criteo.perpetuo.dao.drivers.DriverByName
 import com.criteo.perpetuo.dao.{DbContext, Schema}
 import com.google.inject.{Provides, Singleton}
 import com.twitter.inject.TwitterModule
-import com.typesafe.config.Config
+import com.typesafe.config.{Config, ConfigFactory}
+import com.zaxxer.hikari.{HikariConfig, HikariDataSource}
 import slick.driver.JdbcDriver
 import slick.jdbc.JdbcBackend.Database
 
@@ -14,12 +15,33 @@ class DbContextModule(val config: Config) extends TwitterModule {
   val driverProfile: String = config.getString("driver.profile")
   val driver: JdbcDriver = DriverByName.get(driverProfile)
 
-  private lazy val database = {
-    val jdbcUrl = config.getString("jdbcUrl")
-    val username = config.tryGet[String]("username").orNull
-    val password = config.tryGet[String]("password").orNull
-    Database.forURL(jdbcUrl, driver=driverName, user=username, password=password)
+  private def toHikariConfig(jdbcUrl: String, config: Config) = {
+    val maxPoolSize = config.tryGet[Int]("poolMaxSize").getOrElse(10)
+    val minimumIdle = config.tryGet[Int]("poolMinSize").getOrElse(maxPoolSize)
+    val idleTimeout = config.tryGet[Int]("idleTimeout").getOrElse(600000)
+    val connectionTimeout = config.tryGet[Int]("connectionTimeout").getOrElse(30000)
+    val validationTimeout = config.tryGet[Int]("validationTimeout").getOrElse(5000)
+
+    val hikariConfig = new HikariConfig
+    hikariConfig.setJdbcUrl(jdbcUrl)
+    hikariConfig.setDriverClassName(driverName)
+    hikariConfig.setConnectionTestQuery("SELECT 1;") // Do not rely on Jdbc isValid to check the connection. Some drivers do not implement it.
+    hikariConfig.setPoolName(s"${jdbcUrl}_ConnectionPool")
+    hikariConfig.setMinimumIdle(minimumIdle)
+    hikariConfig.setMaximumPoolSize(maxPoolSize)
+    hikariConfig.setIdleTimeout(idleTimeout)
+    hikariConfig.setConnectionTimeout(connectionTimeout)
+    hikariConfig.setValidationTimeout(validationTimeout)
+
+    hikariConfig
   }
+
+  private lazy val database =
+    Database.forDataSource(
+      new HikariDataSource(
+        toHikariConfig(config.getString("jdbcUrl"), config.tryGetConfig("hikari").getOrElse(ConfigFactory.empty()))
+      )
+    )
 
   // For being overridden, eg for testing
   def databaseProvider: Database = database
