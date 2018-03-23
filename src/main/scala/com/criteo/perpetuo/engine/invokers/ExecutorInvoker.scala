02dc88e282cf5592de54e17b91beb218d093f804
Hugues Lerebours <h.lerebours@criteo.com>
2018-03-23 18:24:40 +0100

    Introduce a type of stoppable `ExecutorInvoker`
    JIRA: DREDD-575
    Change-Id: Ia63f35973aee802488369ff5a228a186920d2a80

--- src/main/scala/com/criteo/perpetuo/engine/invokers/ExecutorInvoker.scala
+++ src/main/scala/com/criteo/perpetuo/engine/invokers/ExecutorInvoker.scala
@@ -1,6 +1,7 @@
 package com.criteo.perpetuo.engine.invokers
 
 import com.criteo.perpetuo.engine.TargetExpr
+import com.criteo.perpetuo.model.ExecutionState.ExecutionState
 import com.criteo.perpetuo.model.Version
 import com.twitter.inject.Logging
 
@@ -11,10 +12,32 @@ trait Trigger {
   def trigger(execTraceId: Long, productName: String, version: Version, target: TargetExpr, initiator: String): Future[Option[String]]
 }
 
+trait Stop {
+  /**
+    * Forcefully stop an execution
+    *
+    * @return the final state of the execution:
+    *         - 'pending' if it is impossible to kill the execution because it is not started yet
+    *         - 'running' if the execution could not be killed and is still seen as running
+    *         - 'unreachable' if it is impossible to interact with the execution (permanently lost)
+    *         - 'killed' if it was successfully stopped while running
+    *         - None if it was already stopped (converted by the caller to 'unresolved' if appropriate)
+    */
+  def stop(execTraceId: Long): Option[ExecutionState]
+}
 
-sealed trait ExecutorInvoker extends Trigger
 
-trait UnstoppableInvoker extends ExecutorInvoker
+sealed trait ExecutorInvoker extends Trigger {
+  val stopper: Option[Long => Option[ExecutionState]]
+}
+
+trait StoppableInvoker extends ExecutorInvoker with Stop {
+  override final val stopper = Some(stop _)
+}
+
+trait UnstoppableInvoker extends ExecutorInvoker {
+  override final val stopper = None
+}
 
 
 // default no-op implementation of an invoker, whose execution is for now seen as running forever
