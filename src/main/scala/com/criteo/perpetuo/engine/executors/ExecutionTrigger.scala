b0b5d2e2d5211c948b484990109ef9cb6bf71d17
Hugues Lerebours <h.lerebours@criteo.com>
2018-03-28 13:57:12 +0200

    Extract dummy execution trigger to dedicated file
    Change-Id: I3d5386f9d1736c2103065422e564842a35caa223

--- src/main/scala/com/criteo/perpetuo/engine/executors/ExecutionTrigger.scala
+++ src/main/scala/com/criteo/perpetuo/engine/executors/ExecutionTrigger.scala
@@ -3,7 +3,6 @@ package com.criteo.perpetuo.engine.executors
 import com.criteo.perpetuo.engine.TargetExpr
 import com.criteo.perpetuo.model.ExecutionState.ExecutionState
 import com.criteo.perpetuo.model.Version
-import com.twitter.inject.Logging
 
 import scala.concurrent.Future
 
@@ -46,18 +45,4 @@ trait TriggeredExecution {
     *         - None if it was already stopped (converted by the caller to 'unresolved' if appropriate)
     */
   val stopper: Option[() => Option[(ExecutionState, String)]]
-}
-
-
-// default no-op implementation of an execution trigger, whose execution is for now seen as running forever
-// todo: add support for synchronous executors to ease the integration of trivial deployments? they would immediately return their final status
-class DummyExecutionTrigger(name: String) extends ExecutionTrigger with Logging {
-  override def toString: String = name
-
-  override def trigger(execTraceId: Long, productName: String, version: Version, target: TargetExpr, initiator: String): Future[Option[String]] = {
-    logger.info(s"Hi, I'm $name! I will run operation #$execTraceId on behalf of: $initiator")
-    Future.successful(None)
-  }
-
-  override val executorName: String = "dummy"
 }
