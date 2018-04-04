03cf42519bcf3ee1fc098c817ce089e1896e36e7
Hugues Lerebours <h.lerebours@criteo.com>
2018-04-04 10:35:48 +0200

    AppConfigProvider: expose helpers to get the executors' configs
    since it will be requested from several locations in the code base
    
    Change-Id: I1e2226347bad7f2a0108d6ff5fabd2b2cbf56d75

--- src/main/scala/com/criteo/perpetuo/config/AppConfigProvider.scala
+++ src/main/scala/com/criteo/perpetuo/config/AppConfigProvider.scala
@@ -6,4 +6,7 @@ object AppConfigProvider {
 
   val config: Config = ConfigFactory.load().resolve()
 
+  val executorsConfig: Config = config.getConfig("executors")
+  def executorConfig(executorName: String): Config = executorsConfig.getConfig(executorName)
+
 }
