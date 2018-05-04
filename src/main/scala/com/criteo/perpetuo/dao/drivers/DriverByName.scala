b0a257929ec99018d5b8a2b661402458038f977a
Hugues Lerebours <h.lerebours@criteo.com>
2018-05-04 19:48:11 +0200

    FixedH2Driver: Remove because the fix has been integrated upstream
    it took effect on our side by the upgrade of Slick
    
    Change-Id: I2abcfc8550175f8ca5bbd506a3d6636ecdf2cf16

--- src/main/scala/com/criteo/perpetuo/dao/drivers/DriverByName.scala
+++ src/main/scala/com/criteo/perpetuo/dao/drivers/DriverByName.scala
@@ -1,12 +1,11 @@
 package com.criteo.perpetuo.dao.drivers
 
-import slick.jdbc.SQLServerProfile
-import slick.jdbc.JdbcProfile
+import slick.jdbc.{H2Profile, JdbcProfile, SQLServerProfile}
 
 
 object DriverByName {
   def get(name: String): JdbcProfile = name match {
-    case "h2" => FixedH2Driver
+    case "h2" => H2Profile
     case "mssql" => SQLServerProfile
   }
 }
