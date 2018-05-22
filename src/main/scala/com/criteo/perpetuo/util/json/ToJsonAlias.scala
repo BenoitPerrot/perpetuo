a38708c128b2d3f0c0334f6b800dacf4bc862a5a
Benoit Perrot <benoit.noe.perrot@gmail.com>
2018-05-22 15:16:32 +0200

    ToJsonAlias: Move from Perpetuo-Criteo
    Change-Id: Iad4fc946e7997668bff1ebcc02bac7058dad1892

---
+++ src/main/scala/com/criteo/perpetuo/util/json/ToJsonAlias.scala
@@ -0,0 +1,26 @@
+package com.criteo.perpetuo.util.json
+
+import spray.json.DefaultJsonProtocol._
+import spray.json._
+
+/**
+  * This class eases the invocation of spray "toJson" from Groovy scripts,
+  * where it is a pain to invoke implicit methods
+  */
+object ToJsonAlias {
+  def deepToJson(m: Map[_, _]): JsObject =
+    m.asInstanceOf[Map[String, _]].mapValues(deepToJson).toJson.asInstanceOf[JsObject]
+
+  def deepToJson(a: Seq[_]): JsArray =
+    a.map(deepToJson).toJson.asInstanceOf[JsArray]
+
+  def deepToJson(v: Any): JsValue = v match {
+    case m: Map[_, _] => deepToJson(m)
+    case a: Seq[_] => deepToJson(a)
+    case s: String => s.toJson
+    case n: BigDecimal => n.toJson
+    case i: Long => i.toJson
+    case b: Boolean => b.toJson
+    case j: JsValue => j
+  }
+}
