54b26dcf432038aea488f1fcf29d66475bb7b5d7
Hugues Lerebours <h.lerebours@criteo.com>
2018-05-28 15:50:44 +0200

    ConfigSyntacticSugar: a cast error displays the usual expected vs got
    Change-Id: I64098ede72ead01a59040a18942eee9008e21aad

--- src/main/scala/com/criteo/perpetuo/config/ConfigSyntacticSugar.scala
+++ src/main/scala/com/criteo/perpetuo/config/ConfigSyntacticSugar.scala
@@ -1,23 +1,30 @@
 package com.criteo.perpetuo.config
 
-import com.typesafe.config.Config
+import com.typesafe.config.{Config, ConfigException}
 
 import scala.collection.JavaConverters._
+import scala.reflect._
 
 object ConfigSyntacticSugar {
 
   implicit class ConfigSyntacticSugar(val config: Config) {
-    def tryGet[T](path: String): Option[T] = if (config.hasPath(path)) Some(get[T](path)) else None
+    def tryGet[T: ClassTag](path: String): Option[T] = if (config.hasPath(path)) Some(get[T](path)) else None
 
-    def getOrElse[T](path: String, default: => T): T = if (config.hasPath(path)) get[T](path) else default
+    def getOrElse[T: ClassTag](path: String, default: => T): T = if (config.hasPath(path)) get[T](path) else default
 
-    def get[T](path: String): T = {
-      config.getValue(path).unwrapped() match {
+    def get[T: ClassTag](path: String): T = {
+      val v = config.getValue(path).unwrapped() match {
         case arr: java.util.ArrayList[_] => arr.asScala
         case obj: java.util.Map[_, _] => obj.asScala
         case value => value
       }
-    }.asInstanceOf[T]
+      try {
+        v.asInstanceOf[T]
+      } catch {
+        case _: ClassCastException =>
+          throw new ConfigException.WrongType(config.origin, s"Expected a ${classTag[T].runtimeClass.getName}, got $v (${v.getClass.getName}")
+      }
+    }
 
     def tryGetConfig(path: String): Option[Config] = if (config.hasPath(path)) Some(config.getConfig(path)) else None
 
