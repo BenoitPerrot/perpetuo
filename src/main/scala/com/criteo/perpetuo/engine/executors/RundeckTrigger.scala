b90fe93c063524e8c6affac8dd2c69b0d19198ff
Remi GUILLARD <r.guillard@criteo.com>
2018-06-08 09:57:28 +0200

    RundeckTrigger: inline ExecutionTrigger abstraction overrides
    The aim is to get away from ExecutionHttpTrigger
    
    Change-Id: Ic6275577d7ffd98d59774a8a52cc9fa0dcaa9d37

--- src/main/scala/com/criteo/perpetuo/engine/executors/RundeckTrigger.scala
+++ src/main/scala/com/criteo/perpetuo/engine/executors/RundeckTrigger.scala
@@ -1,13 +1,18 @@
 package com.criteo.perpetuo.engine.executors
 
 import com.criteo.perpetuo.app.RestApi
-import com.criteo.perpetuo.model.Version
-import com.twitter.finagle.http.{Message, Method, Request}
+import com.criteo.perpetuo.engine.TargetExpr
+import com.criteo.perpetuo.model.{Target, Version}
+import com.twitter.conversions.time._
+import com.twitter.finagle.http.{Message, Method, Request, Status}
+import com.twitter.util.Await
 import com.typesafe.config.Config
 import spray.json.DefaultJsonProtocol._
 import spray.json._
 
 import scala.collection.JavaConverters._
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
 
 
 class RundeckTrigger(name: String,
@@ -74,6 +79,28 @@ class RundeckTrigger(name: String,
           case ERROR_IN_HTML(message) => message
           case _ => ""
         }
+    }
+  }
+
+  override def trigger(execTraceId: Long, productName: String, version: Version, target: TargetExpr, initiator: String): Future[Option[String]] = {
+    // todo: while we only support deployment tactics, we directly give the select dimension, and formatted differently
+    val req = buildRequest(execTraceId, productName, version, Target.getSimpleSelect(target).mkString(","), initiator)
+
+    // trigger the job and return a future to the execution's log href
+    Future {
+      // convert a twitter Future to a scala one
+      val response = Await.result(client(req), requestTimeout + 1.second)
+
+      val content = response.contentString
+      response.status match {
+        case Status.Successful(_) =>
+          val logHref = extractLogHref(content)
+          if (logHref.nonEmpty) Some(logHref) else None
+        case s =>
+          val embeddedDetail = extractMessage(response.statusCode, content)
+          val detail = if (embeddedDetail.nonEmpty) s"${s.reason}: $embeddedDetail" else s.reason
+          throw new Exception(s"Bad response from $this: " + detail)
+      }
     }
   }
 
