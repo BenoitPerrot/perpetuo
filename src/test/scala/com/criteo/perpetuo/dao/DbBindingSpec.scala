aaf37a0ec20ce9b7ea117950b65851a9cd745ce6
Hugues Lerebours <h.lerebours@criteo.com>
2018-06-11 14:08:17 +0200

    DbBindingSpec: Insert simulated effects in the DB
    Change-Id: I41ec3ebf98204d3ead9d6221e41965e444c74dec

--- src/test/scala/com/criteo/perpetuo/dao/DbBindingSpec.scala
+++ src/test/scala/com/criteo/perpetuo/dao/DbBindingSpec.scala
@@ -23,22 +23,51 @@ class DbBindingSpec extends TestHelpers with TestDb {
         (status, kind)
       }
 
+  // to execute before the other tests:
   test("Compute a deployment status: deployment not started") {
     dbScenarios.deploymentPlans
       .map(_.head.deploymentRequest)
       .flatMap(getStatus) should
       eventually(be(DeploymentStatus.notStarted, None))
   }
+
+  // automatic test generation from scenarios:
+  dbScenarios.effects.toStream.foreach(effect =>
+    test(s"Compute a deployment status: ${effect.scenarioName}") {
+      dbScenarios.insertEffect(effect)
+        .flatMap { op =>
+          val depReq = op.deploymentRequest
+          getStatus(depReq).flatMap { case (runningStatus, kind) =>
+            kind shouldEqual Some(effect.kind)
+            dbBinding.closeOperationTrace(op)
+              .flatMap(_ => getStatus(depReq))
+              .map { case (closedStatus, k) =>
+                k shouldEqual kind
+                (runningStatus, closedStatus)
+              }
+          }
+        } should
+        eventually(be(DeploymentStatus.inProgress, DeploymentStatus.withName(effect.deploymentStatus)))
+    }
+  )
 }
 
 
 class DbScenarios(dbBinding: DbBinding) extends DeploymentRequestInserter {
   override val dbContext: DbContext = dbBinding.dbContext
 
-  case class Scenarios(intents: Map[String, List[List[String]]])
+  case class Execution(executionTraceStates: List[String], targetStatuses: List[String])
+
+  case class OperationEffect(scenarioName: String, deploymentStatus: String, planSteps: List[String], executions: List[Execution], private val operation: Option[String]) {
+    def kind: Operation.Kind = operation.map(Operation.withName).getOrElse(Operation.deploy)
+  }
+
+  case class Scenarios(intents: Map[String, List[List[String]]], effects: List[OperationEffect])
 
   object JsonConverters extends DefaultJsonProtocol {
-    implicit val scenariosFormat: RootJsonFormat[Scenarios] = jsonFormat1(Scenarios)
+    implicit val executionFormat: RootJsonFormat[Execution] = jsonFormat2(Execution)
+    implicit val operationFormat: RootJsonFormat[OperationEffect] = jsonFormat5(OperationEffect)
+    implicit val scenariosFormat: RootJsonFormat[Scenarios] = jsonFormat2(Scenarios)
   }
 
 
@@ -47,6 +76,54 @@ class DbScenarios(dbBinding: DbBinding) extends DeploymentRequestInserter {
 
   private val scenarios = Source.fromURL(getClass.getResource("db-scenarios.json")).mkString.parseJson.convertTo[Scenarios]
   val deploymentPlans: Future[Iterable[DeploymentPlan]] = insertIntent()
+  val effects: List[OperationEffect] = scenarios.effects
+
+  private val steps = deploymentPlans.map { plans =>
+    val mapping = plans.flatMap(plan => plan.steps.map(step => step.name -> step))
+    val ret = mapping.toMap
+    assert(ret.size == mapping.size, "All declared steps must be unique in the test scenarios")
+    ret
+  }
+  private val dummySpecIds = Stream.from(0).map(_ =>
+    dbBinding.insertExecutionSpecification("foobar", Version(JsString("version"))).map(_.id)
+  )
+  private val dummyTargets = Iterator.from(100).map(n => s"target-$n")
+
+  def insertEffect(effect: OperationEffect): Future[DeepOperationTrace] =
+    Future
+      .traverse(dummySpecIds.zip(effect.executions)) { case (specId, execution) => specId.map((_, execution)) }
+      .zip(steps)
+      .flatMap { case (specIdsAndExecutions, stepsByName) =>
+        val plan = effect.planSteps.map(stepsByName)
+        val requests = plan.map(_.deploymentRequest).toSet
+        assert(requests.size == 1, "An operation must be bound to steps, which must all be bound to the same deployment request")
+        val deploymentRequest = requests.head
+        val q = dbBinding
+          .insertOperationTrace(deploymentRequest, effect.kind, "a.nonymous")
+          .flatMap(newOp =>
+            dbBinding
+              .insertStepOperationXRefs(plan, newOp)
+              .andThen(
+                dbioTraverse(specIdsAndExecutions) { case (specId, execution) =>
+                  dbBinding
+                    .insertExecution(newOp.id, specId)
+                    .flatMap { executionId =>
+                      dbBinding.insertExecutionTraces(
+                        executionId,
+                        execution.executionTraceStates.map(state => ExecutionTraceRecord(None, executionId, None, ExecutionState.withName(state)))
+                      ).andThen(
+                        dbBinding.updateTargetStatuses(
+                          executionId,
+                          execution.targetStatuses.map(status => dummyTargets.next() -> TargetAtomStatus(Status.withName(status), "")).toMap
+                        )
+                      )
+                    }
+                }
+              )
+              .map(_ => newOp)
+          )
+        dbContext.db.run(q)
+      }
 
   private def insertIntent(): Future[Iterable[DeploymentPlan]] =
     Future
@@ -59,4 +136,7 @@ class DbScenarios(dbBinding: DbBinding) extends DeploymentRequestInserter {
         )
       }
       .map(_.flatten)
+
+  private def dbioTraverse[A, B, E <: Effect](in: Iterable[A])(f: A => DBIOAction[B, NoStream, E]): DBIOAction[Iterable[B], NoStream, E] =
+    DBIO.sequence(in.map(f))
 }
