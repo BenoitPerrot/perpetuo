13e07580903afae99c2067a17072df5be158a6a1
Benoit Perrot <benoit.noe.perrot@gmail.com>
2018-08-25 09:32:11 +0200

    RundeckClient: (fetch) Prefer taking an optional JSON body
    Over building the JSON using a hard coded structure only valid for job
    starting - letting callers (actually there is only one) decide the
    structure of their requests' body.
    
    Change-Id: I835d9e0612056d73fe9d0b9940acbd0779f8904e

--- src/main/scala/com/criteo/perpetuo/engine/executors/RundeckClient.scala
+++ src/main/scala/com/criteo/perpetuo/engine/executors/RundeckClient.scala
@@ -11,7 +11,6 @@ import com.twitter.finagle.http.Status.{NotFound, Ok}
 import com.twitter.finagle.http.{Message, Method, Request, Response}
 import com.twitter.finagle.service.{Backoff, RetryPolicy}
 import com.twitter.util.{Duration, Future => TwitterFuture, Try => TwitterTry}
-import spray.json.DefaultJsonProtocol._
 import spray.json._
 
 class RundeckClient(val host: String) {
@@ -35,21 +34,12 @@ class RundeckClient(val host: String) {
   private val backoffDurations: Stream[Duration] = Backoff.exponentialJittered(1.seconds, 5.seconds).take(5)
   private val backoffPolicy: RetryPolicy[TwitterTry[Nothing]] = RetryPolicy.backoff(backoffDurations)(RetryPolicy.TimeoutAndWriteExceptionsOnly)
 
-  private def fetch(apiSubPath: String, parameters: Map[String, String] = Map()): TwitterFuture[Response] = {
-    // Rundeck before API version 18 does not support invocation with structured arguments
-    val argString = parameters.toStream
-      .map { case (parameterName, value) =>
-        s"-$parameterName $value"
-      }
-      .mkString(" ")
-
-    val body = Map("argString" -> argString).toJson
-
+  private def fetch(apiSubPath: String, body: Option[JsValue] = None): TwitterFuture[Response] = {
     val req = Request(Method.Post, apiSubPath)
     req.host = host
     req.contentType = Message.ContentTypeJson
     req.accept = Message.ContentTypeJson // default response format is XML
-    req.contentString = body.compactPrint
+    req.contentString = body.map(_.compactPrint).getOrElse("")
 
     client(req)
   }
@@ -69,8 +59,18 @@ class RundeckClient(val host: String) {
     if (q.nonEmpty) s"$path?${q.map { case (k, v) => s"$k=$v" }.mkString("&")}" else path
   }
 
-  def startJob(jobName: String, parameters: Map[String, String] = Map()): TwitterFuture[Response] =
-    fetch(apiPath(s"job/$jobName/executions"), parameters)
+  def startJob(jobName: String, parameters: Map[String, String] = Map()): TwitterFuture[Response] = {
+    // Rundeck before API version 18 does not support invocation with structured arguments
+    val body = JsObject(
+      "argString" -> JsString(parameters.toStream
+          .map { case (parameterName, value) =>
+            s"-$parameterName $value"
+          }
+          .mkString(" "))
+    )
+
+    fetch(apiPath(s"job/$jobName/executions"), Some(body))
+  }
 
   private def isJobCompleted(parsedContent: JsObject): Boolean =
     parsedContent.fields("execCompleted").asInstanceOf[JsBoolean].value
