24c6f3114152b59c1da0dbbd401b8eb27b054062
Hugues Lerebours <h.lerebours@criteo.com>
2018-10-10 11:04:01 +0200

    TargetAtomSet: Fix typo
    Change-Id: Id1b4f54faf53875540129ba58a1f534e127517ec

--- src/main/scala/com/criteo/perpetuo/engine/TargetAtomSet.scala
+++ src/main/scala/com/criteo/perpetuo/engine/TargetAtomSet.scala
@@ -7,7 +7,7 @@ final case class TargetAtomSet(items: Set[TargetAtom], isExact: Boolean = true) 
   // - otherwise <=> superset but may contain a "*", so only usable for dispatching (must be considered unresolved elsewhere)
 
   def union(x: TargetAtomSet): TargetAtomSet = {
-    if (isExact != x.items.isEmpty) // fixme: do better than that if something like isExact remains
+    if (isExact != x.isExact) // fixme: do better than that if something like isExact remains
       throw new RuntimeException("Cannot merge two targets of two different resolution levels") // will never occur because they come from the same product
     TargetAtomSet(items.union(x.items), isExact)
   }
