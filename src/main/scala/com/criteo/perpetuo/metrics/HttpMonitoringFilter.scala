df4adfd33889580c297cb7ef958c64143f389e78
Alexandros Stergiakis <a.stergiakis@criteo.com>
2018-11-06 17:53:00 +0100

    metrics.HttpMonitoringFilter: Record latencies on the REST API
    Change-Id: I51cd1da44cb3bd51ff5c1f9d1a89ff4f0dd4dae4
    JIRA:DREDD-339

--- src/main/scala/com/criteo/perpetuo/metrics/HttpMonitoringFilter.scala
+++ src/main/scala/com/criteo/perpetuo/metrics/HttpMonitoringFilter.scala
@@ -3,10 +3,11 @@ package com.criteo.perpetuo.metrics
 import com.samstarling.prometheusfinagle.metrics.Telemetry
 import com.twitter.finagle.http.{Request, Response}
 import com.twitter.finagle.{Service, SimpleFilter}
-import com.twitter.util.Future
-import io.prometheus.client.Counter
+import com.twitter.util.{Future, Stopwatch}
+import io.prometheus.client.{Counter, Histogram}
 
 class HttpMonitoringFilter(telemetry: Telemetry,
+                           buckets: Seq[Double] = Seq(0.001, 0.01, 0.1, 1, 10),
                            labeler: HttpServiceLabeller = new HttpServiceLabeller)
   extends SimpleFilter[Request, Response] {
 
@@ -16,11 +17,23 @@ class HttpMonitoringFilter(telemetry: Telemetry,
     labelNames = labeler.keys
   )
 
+  private val histogram: Histogram = telemetry.histogram(
+    name = "incoming_http_request_latency_seconds",
+    help = "A histogram of the response latency for HTTP requests",
+    labelNames = labeler.keys,
+    buckets = buckets
+  )
+
   override def apply(request: Request,
                      service: Service[Request, Response]): Future[Response] = {
-    service(request).map(response => {
+    val stopwatch = Stopwatch.start()
+      service(request).map(response => {
       if (request.uri.startsWith("/api")) { // filter-out static content
         requests.labels(labeler.labelsFor(request, response): _*).inc()
+
+        histogram
+          .labels(labeler.labelsFor(request, response): _*)
+          .observe(stopwatch().inMilliseconds / 1000.0)
       }
       response
     })
