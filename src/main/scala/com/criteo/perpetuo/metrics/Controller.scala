9325fe7448ef3fde053e7bf851f83c7c23484c24
Alexandros Stergiakis <a.stergiakis@criteo.com>
2018-11-06 18:10:34 +0100

    metrics.Controller: Enable a set of standard JVM metrics
    These are provided by simpleclient_hotspot:
    https://github.com/prometheus/client_java/tree/master/simpleclient_hotspot/src/main/java/io/prometheus/client/hotspot
    
    Change-Id: I4ec27c45936e650607020b5d23dfd67a8d5c6631
    JIRA:DREDD-339

--- src/main/scala/com/criteo/perpetuo/metrics/Controller.scala
+++ src/main/scala/com/criteo/perpetuo/metrics/Controller.scala
@@ -4,11 +4,13 @@ import com.samstarling.prometheusfinagle.metrics.MetricsService
 import com.twitter.finagle.http.Request
 import com.twitter.finatra.http.{Controller => BaseController}
 import io.prometheus.client.CollectorRegistry
+import io.prometheus.client.hotspot.DefaultExports
 import javax.inject.Singleton
 
 @Singleton
 class Controller
   extends BaseController {
+  DefaultExports.initialize
   val registry: CollectorRegistry = CollectorRegistry.defaultRegistry
   val metricsService: MetricsService = new MetricsService(registry)
   get("/metrics") { r: Request =>
