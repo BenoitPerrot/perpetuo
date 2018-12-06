e8666db120ecd8bb16e2d82c04cd12fcf88b8150
Majdi Haouech <m.haouech@criteo.com>
2018-12-06 10:05:52 +0100

    DeploymentRequestState: Add the missing states
    to be used in future commits
    
    Change-Id: I7244eee0f87f72b5c3befb0c7cb71549dcbe9e2a

--- src/main/scala/com/criteo/perpetuo/model/DeploymentRequestState.scala
+++ src/main/scala/com/criteo/perpetuo/model/DeploymentRequestState.scala
@@ -3,5 +3,14 @@ package com.criteo.perpetuo.model
 object DeploymentRequestState extends Enumeration {
   type Code = Value
 
+  val notStarted = Value(0, "notStarted")
   val abandoned = Value(1, "abandoned")
+  val deployInProgress = Value(2, "deployInProgress")
+  val revertInProgress = Value(3, "revertInProgress")
+  val deployFailed = Value(4, "deployFailed")
+  val revertFailed = Value(5, "revertFailed")
+  val deployFlopped = Value(6, "deployFlopped")
+  val deployed = Value(7, "deployed")
+  val reverted = Value(8, "reverted")
+  val paused = Value(9, "paused")
 }
