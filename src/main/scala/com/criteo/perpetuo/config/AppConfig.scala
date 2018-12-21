ec51bccdd76b496d5d06bac1b59ac1f46f5c64a4
Benoit Perrot <be.perrot@criteo.com>
2018-12-21 10:24:41 +0100

    AppConfig: Introduce class along with companion
    Change-Id: Ie56c0ef6cd49d96b5d5faa0bc857c49adb9445dd

--- src/main/scala/com/criteo/perpetuo/config/AppConfig.scala
+++ src/main/scala/com/criteo/perpetuo/config/AppConfig.scala
@@ -2,13 +2,12 @@ package com.criteo.perpetuo.config
 
 import com.typesafe.config.{Config, ConfigFactory}
 
-object AppConfig {
-
-  val config: Config = ConfigFactory.load().resolve()
-
+class AppConfig(val config: Config) {
   val selfUrl: String = config.getString("selfUrl")
 
   val executorsConfig: Config = config.getConfig("executors")
   def executorConfig(executorName: String): Config = executorsConfig.getConfig(executorName)
 
 }
+
+object AppConfig extends AppConfig(ConfigFactory.load().resolve())
