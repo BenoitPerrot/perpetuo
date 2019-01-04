c829d234173b7418b9548b35525c2da56061579d
Majdi Haouech <m.haouech@criteo.com>
2019-01-04 17:03:36 +0100

    Scheduler: Return a java Future
    instead of a Scheduled future
    
    Change-Id: Ie2156ad787f88aa2ee576989ade9c65830a93606

--- src/main/scala/com/criteo/perpetuo/engine/Scheduler.scala
+++ src/main/scala/com/criteo/perpetuo/engine/Scheduler.scala
@@ -1,6 +1,6 @@
 package com.criteo.perpetuo.engine
 
-import java.util.concurrent.{Executors, ScheduledFuture, TimeUnit}
+import java.util.concurrent.{Executors, Future, TimeUnit}
 
 import com.google.inject.Singleton
 
@@ -9,7 +9,7 @@ class Scheduler {
   private val THREAD_COUNT = 2 // TODO: Increase the thread count relatively to the number of tasks that need to be executed
   private val executor = Executors.newScheduledThreadPool(THREAD_COUNT)
 
-  def scheduleTask(f: () => Any, period: Long, timeUnit: TimeUnit, initialDelay: Long = 0): ScheduledFuture[_] = {
+  def scheduleTask(f: () => Any, period: Long, timeUnit: TimeUnit, initialDelay: Long = 0): Future[_] = {
     executor.scheduleAtFixedRate(new Runnable {
       override def run(): Unit = f()
     }, initialDelay, period, timeUnit)
